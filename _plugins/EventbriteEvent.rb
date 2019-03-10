7c7e34a53d5f559e3da9bdb3ea48110ed87900b5
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2019-03-10 14:58:44 -0500

    Add in address for coming up next event. Required api call to venues end point.

--- _plugins/EventbriteEvent.rb
+++ _plugins/EventbriteEvent.rb
@@ -18,6 +18,14 @@ module Jekyll
             @event_info = JSON.parse(@data.body)
             @event_date = @event_info["events"][0]["start"]["local"]
             @parsed_date = DateTime.parse(@event_date)
+
+            venue_uri = URI.parse("https://www.eventbriteapi.com/v3/venues/30711856/?token=#{ygl_eb_token}")
+            https = Net::HTTP.new(uri.host, uri.port)
+            https.use_ssl = true
+
+            @venue_data = https.get(venue_uri)
+            @venue_info = JSON.parse(@venue_data.body)
+
         
             return "
             <div class='event'>
@@ -25,6 +33,7 @@ module Jekyll
                 <h4>#{@parsed_date.strftime("%A %b %d at %I:%M %p")}</h4>
                 <h3>#{@event_info["events"][0]["name"]["text"]}</h3>
                   <p>#{@event_info["events"][0]["description"]["text"]}.</p>
+                  <p>#{@venue_info["address"]["localized_address_display"]}</p>
                  <p><a class='button' href='#{@event_info["events"][0]["url"]}'>RSVP</a>
                   <a href='#{@event_info["events"][0]["url"]}'>Details</a></p>
                 </div>
